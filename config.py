6dd65f428e7d7f0001d4fd3bad343b6f477333a0
mkkh01 <mk2002186mk@gmail.com>
2026-06-21 17:49:36 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@
 # 1. التوكن الخاص بك
 TELEGRAM_TOKEN = "8913262863:AAFgwkszxhOLpE4IX874HrOKUb1FZUZwYSo"
 
-# 2. الرابط الجديد من خانة Transaction pooler (الذي يدعم IPv4)
-RAW_DATABASE_URL = "postgresql://postgres:C,TWTpTrK+7#mp.@db.lvvcbqqtjygqlxyhiabm.supabase.co:5432/postgres"
+# 2. الرابط الجديد من خانة Session pooler (الذي يدعم IPv4)
+RAW_DATABASE_URL = "postgresql://postgres.lvvcbqqtjygqlxyhiabm:C,TWTpTrK+7#mp.@aws-1-eu-central-1.pooler.supabase.com:5432/postgres"
 
 # تحويل الرابط ليدعم asyncpg
 # ملاحظة: تم إزالة ?ssl=require من الرابط واستخدام SSLContext في database.py لضمان توافق أعلى مع Render و Supabase
